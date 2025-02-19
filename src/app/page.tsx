98bd1308139ca0d32a6ce60c9ab1a6c95a3d9b9a
José <msreynaldojose@gmail.com>
2025-02-18 22:22:57 -0300

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { ArrowRight, Copy, Mail } from "lucide-react";
+import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div className="text-white">Hello world!</div>
+      <Button type="submit">
+        Enviar
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <InputRoot>
+        <InputIcon>
+          <Mail />
+        </InputIcon>
+        <InputField />
+      </InputRoot>
     </main>
   );
 }
