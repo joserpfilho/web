1a6967b70127a370b0b4f4ee193a767a1c7cb8d8
joserpfilho <msreynaldojose@gmail.com>
2025-02-20 14:08:54 -0300

    Add invite link prop to InviteLinkInput component and set default invite link in InvitePage

--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,15 +1,27 @@
+'use client';
+
 import { IconButton } from '@/components/icon-button';
 import { InputField, InputIcon, InputRoot } from '@/components/input';
 import { Copy, Link } from 'lucide-react';
 
-export function InviteLinkInput() {
+interface InviteLinkInputProps {
+	inviteLink: string;
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+	function copyInviteLink(): void {
+		console.log('Copied invite link to clipboard');
+	}
+
 	return (
 		<InputRoot>
 			<InputIcon>
 				<Link className="size-5" />
 			</InputIcon>
-			<InputField readOnly defaultValue={'teste input'} />
-			<IconButton className="-mr-2">
+
+			<InputField readOnly defaultValue={inviteLink} />
+
+			<IconButton className="-mr-2" onClick={copyInviteLink}>
 				<Copy />
 			</IconButton>
 		</InputRoot>
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -6,6 +6,8 @@ import { Ranking } from './ranking';
 import { Stats } from './stats';
 
 export default function InvitePage() {
+	const inviteLink = 'http://localhost:3000/invite/asijd782heo-1f9012jf98';
+
 	return (
 		<div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
 			<div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -31,7 +33,7 @@ export default function InvitePage() {
 						</p>
 					</div>
 				</div>
-				<InviteLinkInput />
+				<InviteLinkInput inviteLink={inviteLink} />
 				<Stats />
 			</div>
 			<Ranking />
