bbee2d30de50ea4a59bfaa81b0aea70f5dba673d
joserpfilho <msreynaldojose@gmail.com>
2025-02-20 13:44:34 -0300

    Refactor invite page to use separate Ranking component for improved structure and readability

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -4,10 +4,8 @@ import { IconButton } from '@/components/icon-button';
 import { InputField, InputIcon, InputRoot } from '@/components/input';
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react';
 
-import cooper from '../../assets/cooper.svg';
 import logo from '../../assets/logo.svg';
-import gold from '../../assets/medal-gold.svg';
-import silver from '../../assets/silver.svg';
+import { Ranking } from './ranking';
 
 export default function InvitePage() {
 	return (
@@ -80,23 +78,7 @@ export default function InvitePage() {
 				</div>
 			</div>
 
-			<div className="w-full max-w-[440px] space-y-5">
-				<h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
-					Ranking de indicações
-				</h2>
-
-				<div className="space-y-4">
-					<div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-						<span className="text-sm text-gray-300 leading-none">
-							José Reynaldo
-						</span>
-						<span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-							1000
-						</span>
-						<Image src={gold} alt="" className="absolute top-0 right-8" />
-					</div>
-				</div>
-			</div>
+			<Ranking />
 		</div>
 	);
 }
